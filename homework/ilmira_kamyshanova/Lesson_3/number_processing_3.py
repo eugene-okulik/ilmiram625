801d733334b59230341d2c938f24d6d7f3eec33f
Ilmira Idea <ilmiram625@gmail.com>
2025-03-27 15:42:46 +0300

    Ilmira Kamyshanova: homework 3: done

---
+++ homework/ilmira_kamyshanova/Lesson_3/number_processing_3.py
@@ -0,0 +1,6 @@
+number_1 = 13
+number_2 = 25
+print(f'Arithmetical average of numbers number_1 = {number_1} and number_2 = {number_2} '
+      f'is: {(number_1 + number_2) / 2}')
+print(f'Geometrical average of numbers number_1 = {number_1} and number_2 = {number_2} '
+      f'is: {(number_1 * number_2) ** 0.5}')
