57fdd3c6a9845dbd656f8ebeb44d08f36a169936
Ilmira Idea <ilmiram625@gmail.com>
2025-04-02 23:19:35 +0300

    Ilmira Kamyshanova: homework 7: done: string_methods_with_functions.py fixed

--- homework/ilmira_kamyshanova/Lesson_7/string_methods_with_functions.py
+++ homework/ilmira_kamyshanova/Lesson_7/string_methods_with_functions.py
@@ -2,9 +2,11 @@ text_1 = 'результат операции: 42'
 text_2 = 'результат операции: 514'
 text_3 = 'результат работы программы: 9'
 
+
 def print_result(text: str):
     number = int(text.split()[-1])
     print(f'Summ result for {number}: {number + 10}')
+
 
 print_result(text_1)
 print_result(text_2)
