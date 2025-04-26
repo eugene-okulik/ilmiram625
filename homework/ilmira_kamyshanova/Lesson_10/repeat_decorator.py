75f38fbf2cb48b052a7fc0006c74d2d18fe0dbe1
Ilmira Idea <ilmiram625@gmail.com>
2025-04-26 19:10:00 +0300

    Ilmira Kamyshanova: homework 10: done

---
+++ homework/ilmira_kamyshanova/Lesson_10/repeat_decorator.py
@@ -0,0 +1,29 @@
+def repeat_me(func):
+
+    def wrapper(*args, count):
+        for i in range(count):
+            func(*args)
+
+    return wrapper
+
+@repeat_me
+def example(text):
+    print(text)
+
+example('print me', count=2)
+
+
+def repeat_me_2(count):
+    def decorator(func):
+        def wrapper(*args):
+            for i in range(count):
+                func(*args)
+
+        return wrapper
+    return decorator
+
+@repeat_me_2(count=2)
+def example_2(text):
+    print(text)
+
+example_2('print me')
