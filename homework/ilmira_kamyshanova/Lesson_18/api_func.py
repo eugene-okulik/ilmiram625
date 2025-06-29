a3abf52604458ec5f0b2d502f4e4183332dbf43a
Ilmira Idea <ilmiram625@gmail.com>
2025-06-29 21:44:27 +0300

    Ilmira Kamyshanova: homework 18: fixed

--- homework/ilmira_kamyshanova/Lesson_18/api_func.py
+++ homework/ilmira_kamyshanova/Lesson_18/api_func.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def new_object():
     body = {
         "data": {
@@ -11,8 +12,10 @@ def new_object():
     response = requests.post('http://objapi.course.qa-practice.com/object', json=body)
     return response.json()['id']
 
+
 def clear(delete_id):
     requests.delete(f'http://objapi.course.qa-practice.com/object/{delete_id}')
+
 
 def post_object():
     body = {
@@ -30,6 +33,7 @@ def post_object():
     assert response.json()['data'] == body['data']
     clear(response.json()['id'])
 
+
 def put_object():
     put_id = new_object()
     body = {
@@ -45,6 +49,7 @@ def put_object():
     assert response.json()['data'] == body['data']
     clear(put_id)
 
+
 def patch_object():
     patch_id = new_object()
     body = {
@@ -58,10 +63,12 @@ def patch_object():
     assert response.json()['data'] == body['data']
     clear(patch_id)
 
+
 def delete_object():
     delete_id = new_object()
     response = requests.delete(f'http://objapi.course.qa-practice.com/object/{delete_id}')
     assert response.status_code == 200, f'Status code is "{response.status_code}", not 200!'
+
 
 post_object()
 put_object()
