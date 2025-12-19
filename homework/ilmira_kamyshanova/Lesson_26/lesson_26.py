bba8e2e3c2bbdfa52c50da567851255a23563d80
Ilmira Idea <ilmiram625@gmail.com>
2025-12-19 09:10:13 +0300

    Ilmira Kamyshanova: homework 26: fixed

--- homework/ilmira_kamyshanova/Lesson_26/lesson_26.py
+++ homework/ilmira_kamyshanova/Lesson_26/lesson_26.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import pytest
-from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -11,14 +10,13 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture()
 def driver():
     chrome_driver = webdriver.Chrome()
-    sleep(40)
     chrome_driver.maximize_window()
     yield chrome_driver
-    sleep(3)
-
+    chrome_driver.quit()
 
 def test_item_in_cart_new_tab(driver):
     driver.get('http://testshop.qa-practice.com')
+    driver.implicitly_wait(15)
     wait = WebDriverWait(driver, 100)
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[class="text-primary text-decoration-none"]')))
     chosen_item = driver.find_element(By.CSS_SELECTOR, 'a[class="text-primary text-decoration-none"]')
@@ -28,7 +26,6 @@ def test_item_in_cart_new_tab(driver):
     driver.switch_to.window(tabs[1])
     driver.find_element(By.ID, 'add_to_cart').click()
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="btn btn-secondary"]'))).click()
-    sleep(3)
     driver.close()
     driver.switch_to.window(tabs[0])
     driver.refresh()
@@ -39,14 +36,13 @@ def test_item_in_cart_new_tab(driver):
 
 def test_item_in_cart(driver):
     driver.get('http://testshop.qa-practice.com')
+    driver.implicitly_wait(10)
     wait = WebDriverWait(driver, 100)
     actions = ActionChains(driver)
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'img[alt="Customizable Desk"]')))
     chosen_item = driver.find_element(By.CSS_SELECTOR, 'a[class="btn btn-primary a-submit"]')
-    sleep(2)
     actions.move_to_element(chosen_item).perform()
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[class="btn btn-primary a-submit"]'))).click()
     item_in_cart = wait.until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'strong.product-name.product_display_name')))
-    sleep(3)
     assert chosen_item.text in item_in_cart.text
